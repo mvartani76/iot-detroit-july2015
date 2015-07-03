31d0a00c82607bfe830215082dfdbc1066fdb433
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-03 13:55:25 -0400

    Entered DB info except password

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 function Connection(){
-   if (!($link=mysqli_connect("your_ipserver","your_user","your_pass")))  {
+   if (!($link=mysqli_connect("37.60.225.35","iotdetro_dbusr","password")))  {
       exit();
    }
-   if (!mysql_select_db("your_database",$link)){
+   if (!mysql_select_db("iotdetro_arduino-iot-test-july2015",$link)){
       exit();
    }
    return $link;
