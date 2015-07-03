84d8109b19a2cd28be2076e4c83f36add3ac2d5e
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-03 13:46:10 -0400

    changed mysql_connect() to mysqli_connect() as the former has been deprecated in php5.5.0+

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,6 @@
 <?php
 function Connection(){
-   if (!($link=mysql_connect("your_ipserver","your_user","your_pass")))  {
+   if (!($link=mysqli_connect("your_ipserver","your_user","your_pass")))  {
       exit();
    }
    if (!mysql_select_db("your_database",$link)){
