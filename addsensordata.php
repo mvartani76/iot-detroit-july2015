111ad5c8bd9401a834a71897d7d17a1e4ceea564
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-06 16:54:29 -0400

    Adding error checking to see if data is inserted into table

--- addsensordata.php
+++ addsensordata.php
@@ -2,6 +2,12 @@
    	include("db_connection.php");
    	$link=Connection();
 	$Sql="INSERT INTO sensordata (temp1,photo1) VALUES ('".$_GET["temp1"]."', '".$_GET["photo1"]."')";     
-   	mysqli_query($link,$Sql);
+
+	if (mysqli_query($link, $Sql)) {
+    	echo "New record created successfully";
+	} else {
+    	echo "Error: " . $Sql . "<br>" . mysqli_error($link);
+	}
+
    	header("Location: displaysensordata.php");
 ?>
