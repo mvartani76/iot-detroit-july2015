c9dc063a443732e6c8811f064d1d8c5f253e5f0a
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-06 17:43:30 -0400

    debugging db connection

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,21 @@
 <?php
 function Connection(){
-	if 
-(!($link=mysqli_connect("localhost","iotdetro_dbusr","password")))  
-{
-		echo "Could not connect to mysql..." . mysqli_connect_error();
+	if (!($link=mysqli_connect("localhost","iotdetro_dbusr","password")))  
+	{
+		echo "Could not connect to mysqli..." . mysqli_connect_error();
 		exit();
 	}
+	else
+	{
+		echo "Connected to mysqli...\n";
+	}
 	if (!mysqli_select_db($link, "iotdetro_arduino_iot_test_july2015")){
-		echo "Could not connect to mysql dbsss..." . mysqli_error();
+		echo "Could not connect to mysql db..." . mysqli_error();
 		exit();
+	}
+	else
+	{
+		echo "Connected to db...\n";
 	}
 	return $link;
 }
