83a1fb3ec45ef2d7f017c3f0febc94e69e9a0d2a
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-05 20:49:49 -0400

    Corrected database name?

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,13 @@
 <?php
 function Connection(){
-	if (!($link=mysqli_connect("37.60.225.35","iotdetro_dbusr","password")))  {
-		echo "Could not connect to mysql...";
+	if 
+(!($link=mysqli_connect("localhost","iotdetro_dbusr","password")))  
+{
+		echo "Could not connect to mysql..." . mysqli_connect_error();
 		exit();
 	}
-	if (!mysql_select_db("iotdetro_arduino-iot-test-july2015",$link)){
-		echo "Could not connect to mysql...";
+	if (!mysqli_select_db($link, "iotdetro_arduino_iot_test_july2015")){
+		echo "Could not connect to mysql dbsss..." . mysqli_error();
 		exit();
 	}
 	return $link;
