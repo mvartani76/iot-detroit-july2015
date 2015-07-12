1e8ced960a43617fd34517465a0ff41d4d4e0e89
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-11 23:20:02 -0400

    Moved sensor read php code to new directory within the PHP folder

---
+++ php/SensorRead_MySQL/displaysensordata.php
@@ -0,0 +1,40 @@
+<html>
+   <head>
+      <title>Data of Sensor</title>
+   </head>
+   <body>
+      <h1>Data from the temperature and photo sensors</h1>
+<!--
+         <form method="get" action="addsensordata.php">
+            <table>
+               <tr>
+                  <td>Temperature 1</td>
+                  <td><input type="text" name="temp1" size="20" maxlength="30"></td>
+               </tr>
+               <tr>
+                  <td>Photo Sensor 1</td>
+                  <td><input type="text" name="photo1" size="20" maxlength="30"></td>
+               </tr>
+            </table>
+            <input type="submit" name="record_button" value="Record">
+         </form> -->
+         <hr>
+      <?php
+         include("db_connection.php");
+         $link=Connection();
+         $result=mysqli_query($link,"select * from sensordata order by id desc");
+      ?>
+      <table border="1" cellspacing="1" cellpadding="1">
+         <tr>
+            <td>&nbsp;Temperature 1&nbsp;</td>
+            <td>&nbsp;Photo Sensor 1&nbsp;</td>
+          </tr>
+         <?php     
+            while($row = mysqli_fetch_array($result)) {
+               printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td></tr>", $row["temp1"], $row["photo1"]);
+            }
+            mysqli_free_result($result);
+         ?>
+      </table>
+   </body>
+</html>
