ea6fa89be8c8c8623b5a64a0f9fb2475619d3170
Mike Vartanian <mike.vartanian@gmail.com>
2015-07-13 18:19:19 -0400

    Added DeviceType to DB

--- php/SensorRead_MySQL/addsensordata.php
+++ php/SensorRead_MySQL/addsensordata.php
@@ -1,7 +1,7 @@
 <?php
    	include("db_connection.php");
    	$link=Connection();
-	$Sql="INSERT INTO sensordata (temp1,photo1) VALUES ('".$_GET["temp1"]."', '".$_GET["photo1"]."')";     
+	$Sql="INSERT INTO sensordata (temp1,photo1,DeviceType) VALUES ('".$_GET["temp1"]."', '".$_GET["photo1"]."', '".$_GET["DeviceType"]."')";     
 
 	if (!mysqli_query($link, $Sql)) {
     	echo "Error: " . $Sql . "<br>" . mysqli_error($link);
